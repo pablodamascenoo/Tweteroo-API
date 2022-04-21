49f346dba0cd5599bec1e2cac11e2f24b50ac0c8
pablodamascenoo <pablofacex@gmail.com>
2022-04-21 12:18:38 -0300

    feat: added sign-up route

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 import express from "express";
 import cors from "cors";
 import chalk from "chalk";
+import bodyParser from "body-parser";
+
+let users = [];
+let tweets = [];
 
 const app = express();
 app.use(cors());
 
+let jsonParser = bodyParser.json();
+
 app.listen(5000, () => {
   console.log(chalk.bold.cyan("\nRunning server...\n"));
+});
+
+app.post("/sign-up", jsonParser, (req, res) => {
+  const { username, avatar } = req.body;
+
+  users.push({ username, avatar });
+  console.log(users);
+  res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+  res.send(tweets);
+});
+
+app.post("/tweets", jsonParser, (req, res) => {
+  const { username, tweet } = req.body;
+
+  let findUser = users.find((user) =>
+    user.username === username ? true : false
+  );
+
+  tweets.push({
+    username,
+    tweet,
+    avatar: findUser.avatar,
+  });
+
+  res.send(tweets);
 });
